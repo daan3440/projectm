700c89e84476621f1a0d03a1fffc84dd7ca0fab3
daan3440 <daan3440@student.su.se>
2019-05-09 12:01:06 +0200

    UserTrails Repo

--- src/main/java/pvt73app/MYSQL/UserTrails.java
+++ src/main/java/pvt73app/MYSQL/UserTrails.java
@@ -1,14 +1,14 @@
 package pvt73app.MYSQL;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
 
 public class UserTrails {
-    @Id
+    
+
+	@Id
 //    @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int tid;
     private int uid;
