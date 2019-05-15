4edc7fdfcbf595abde038429140caad91f825e07
AntonShad <anton.harneby@gmail.com>
2019-05-15 17:49:01 +0200

    Getting userTrails and updating them should now be working

--- src/main/java/pvt73app/MYSQL/UserTrailsRepository.java
+++ src/main/java/pvt73app/MYSQL/UserTrailsRepository.java
@@ -4,11 +4,13 @@
 package pvt73app.MYSQL;
 
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface UserTrailsRepository extends CrudRepository<UserTrails, Integer> {
-	Optional<UserTrails> findByUid(Integer uid);
+	List<UserTrails> findByUid(Integer uid);
+	List<UserTrails> findByFavourite(boolean favourite);
 
 }
