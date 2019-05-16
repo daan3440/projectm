a926318a9056dd3f4100751bb5e4032790cdd338
daan3440 <daan3440@student.su.se>
2019-05-16 10:37:25 +0200

    UserGroup FIX

---
+++ src/main/java/pvt73app/MYSQL/UserGroup.java
@@ -0,0 +1,28 @@
+package pvt73app.MYSQL;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// This tells Hibernate to make a table out of this class
+@Entity(name = "usergroup")
+public class UserGroup {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String groupname;
+
+	public Integer getID() {
+		return id;
+	}
+	public String getGroupName(){
+		return groupname;
+	}
+
+	public void setGroupName(String groupname){
+		this.groupname=groupname;
+	}
+
+}
