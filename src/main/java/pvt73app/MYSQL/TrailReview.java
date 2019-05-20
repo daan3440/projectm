576276cc5040dcaa3afa00c85e39819be1929ad3
AntonShad <anton.harneby@gmail.com>
2019-05-20 13:46:54 +0200

    Composit key now working

--- src/main/java/pvt73app/MYSQL/TrailReview.java
+++ src/main/java/pvt73app/MYSQL/TrailReview.java
@@ -2,19 +2,27 @@ package pvt73app.MYSQL;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 
 import java.time.LocalDateTime;
 
 // This tells Hibernate to make a table out of this class
 @Entity(name = "trailreview")
+@IdClass(TrailReviewID.class)
 public class TrailReview implements FeedElement{
+	@Id
+	@Column(name = "tid")
 	private int tid; // TODO: Make tid and uid into a composit key to make it work in the future
-    @Id
-    private int uid;
+	@Id
+	@Column(name = "uid")
+	private int uid;
+	//private TrailReviewID id;
 	private String review;
 	private int rating;
 	private LocalDateTime date;
@@ -25,14 +33,14 @@ public class TrailReview implements FeedElement{
 	}
 
 	public void setTid(int tid){
-		this.tid=tid;
+		this.tid = tid;
 	}
 	public int getUid(){
 		return uid;
 	}
 	
 	public void setUid(int uid){
-		this.uid=uid;
+		this.uid = uid;
 	}
 
 	public String getReview(){
---
+++ src/main/java/pvt73app/MYSQL/TrailReviewID.java
@@ -0,0 +1,11 @@
+package pvt73app.MYSQL;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+public class TrailReviewID implements Serializable {
+    private int uid;
+    private int tid;
+}
